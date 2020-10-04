b2fb5637e2b8c18450f260ab8efaec280a144132
elducati <geomondi09@gmail.com>
2020-10-04 11:35:48 +0300

    created ToDoItem.js, added elements, styled the lements

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ToDoItem from './TodoItem';
 
 function App() {
   return (
-    <div className="App">
-      <input type="checkbox" />
-            <p>Placeholder text here</p>
-            
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
-            
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
-            
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
+    <div className="todo-item">
+    <ToDoItem />
     </div>
   );
 }
---
+++ src/TodoItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+function ToDoItem() {    
+    return (
+        <div className="App todo-list">
+            <input type="checkbox" />
+            <p>this is a placeholder</p>
+            <input type="checkbox" />
+            <p>this is a placeholder</p>
+            <input type="checkbox" />
+            <p>this is a placeholder</p>
+            <input type="checkbox" />
+            <p>this is a placeholder</p>
+            <input type="checkbox" />
+            <p>this is a placeholder</p>
+        </div>
+    );   
+
+}
+export default ToDoItem;
