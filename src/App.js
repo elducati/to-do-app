dec80668a16e8bf4e15892fda630d57d21423599
elducati <geomondi09@gmail.com>
2020-10-04 17:55:04 +0300

    added props and a data file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import ToDoItem from './TodoItem';
+import todosData from "./todosData";
 
 function App() {
+  const todoItems = todosData.map(item => <ToDoItem key={item.id} item={item} />)
   return (
-    <div className="todo-item">
-    <ToDoItem />
+    <div className="todo-list">
+      {todoItems}
     </div>
   );
 }
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,21 +1,13 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-function ToDoItem() {    
+function ToDoItem(props) {
     return (
-        <div className="App todo-list">
-            <input type="checkbox" />
-            <p>this is a placeholder</p>
-            <input type="checkbox" />
-            <p>this is a placeholder</p>
-            <input type="checkbox" />
-            <p>this is a placeholder</p>
-            <input type="checkbox" />
-            <p>this is a placeholder</p>
-            <input type="checkbox" />
-            <p>this is a placeholder</p>
+        <div className="todo-item">
+            <input type="checkbox" checked={props.item.completed}/>
+            <p>{props.item.text}</p>
         </div>
-    );   
+    );
 
 }
 export default ToDoItem;
